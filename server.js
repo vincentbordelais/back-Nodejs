7d1482a58d1992db3fd39fd813179676f50ff626
bordelais <vbordelais@affinisoft.com>
2019-11-06 15:38:13 +0100

    Coonexion BDD et rép auth

--- server.js
+++ server.js
@@ -3,24 +3,54 @@ const express = require('express')
 const app = express()
 var cors = require('cors') // gère les problèmes liés au CORS (Cross Origin Resource Sharing)
 var bodyParser = require('body-parser')  // aide à récupérer les informations des demandes POST, le body
+const mysql = require('mysql')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const SECRET_KEY = "secretkey23456";
+const router = express.Router();
+
 
 // Middlewares :
 app.use(cors())
 app.use(bodyParser.json());
 
-/*
-// Routes :
-app.get('/membres', (req, res) => {
-    // res.send('Page Membres')
-    res.json("Ceci est mon texte")
-}) */
+router.use(bodyParser.urlencoded({ extended:  false }));
+router.use(bodyParser.json());
 
-app.listen(8080, () => {
-        console.log('Started on port 8080.')
+// Fonctions :
+const findUserByEmail = (email, cb) => {
+    return connection.query(`SELECT * FROM user WHERE email = ?`,[email], (err, row) => {
+            cb(err, row[0])
+            console.log(row[0]); // Users renvoyés par la bdd, [] si pas trouvé.
+    });
+}
+ 
+// Routes :
+router.post('/login', (req, res) => {
+    // Récupération du formulaire :
+    const email = req.body[0];
+    console.log("Réception par le server :"+ email);
+   
+    // Réponse envoyée à l'appli :
+    findUserByEmail(email, (err, user) => {
+        //console.log(user);
+        if (err) return res.status(500).send('Erreur du Serveur!');
+        if (user == []) return res.status(404).send('User not found'); // cette condition ne marche jamais
+        // res.json(user);
+        const expiresIn = 24 * 60 * 60;
+        const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, {
+            expiresIn: expiresIn
+        });
+        res.status(200).send({ "user venant du serveur : ": user, "access_token": accessToken, "expires_in": expiresIn});
+    }); 
+});
+
+router.get('/', (req, res) => {
+        // res.send('Page d\'accueil')
+        res.json("Voici le serveur d'authentification  des utilisateurs de l'application Syvios.")
     })
 
-
-const mysql = require('mysql')
+// BDD :
 // Initialisation de la connexion :
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -36,58 +66,17 @@ connection.connect((err) => {
         } else {
                 console.log("La base de données est connectée.");
         }
-    })
-/*
-// Requête à la bdd :
-connection.query("SELECT * FROM user", (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        // Route membres :
-        app.get('/membres', (req, res) => {
-            res.json(result)
-        })    
-    }
-}) */
+})
 
-
-// Configuration de la route où notre application peut adresser des demandes post :
-app.post('/login', (req, res) => {
-    // console.log(req.body);
-    const email = req.body[0];
-    const password = req.body[1];
-    
-    connection.query("SELECT * FROM user WHERE email='" + email + "' AND password='" + password + "'", (err, rows, fields) => {
-        if (err) throw err;
-        res.json(rows[0]);
-    })       
+// Port :
+app.use(router);
+const  port  =  process.env.PORT  ||  8080;
+const  server  =  app.listen(port, () => {
+    console.log('Server listening at http://localhost:'  +  port);
 });
 
-
-
-
 /*
-// Configuration de la route où notre application peut adresser des demandes post :
-app.post('/login', (req, res) => {
-    // console.log(req.body);
-    const email = req.body[0];
-    const password = req.body[1];
-    
-    // Requête à la bdd :
-    connection.query("SELECT * FROM user WHERE email='" + email + "' AND password='" + password + "'", (err, result) => {
-        if (err) {
-            console.log(err.message);
-            // console.log("NOT OK"); 
-        } else {    
-            console.log(result);
-            if (result = null){
-                // res.json({"success": false});
-                console.log("NOT OK");
-            } else {
-                // res.json({"success": true});
-                console.log("OK");
-            }
-        }  
+app.listen(8080, () => {
+        console.log('Ce serveur se lance sur le port 8080.')
     })
-});
 */
